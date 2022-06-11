677bcc8683134bfeadfb8002075290cec6addac6
leah karienye <karienyeleah@gmail.com>
2022-06-11 21:58:05 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, customer) {
+    const names = drivers.filter 
+    (driver => driver.toUpperCase() === customer.toUpperCase());
+    return names;
+    }
+  
+  function fuzzyMatch(drivers, customer) {
+    const names = drivers.filter 
+    (driver => driver.toLowerCase().indexOf (customer.toLowerCase())=== 0 );
+    return names;
+    }
+  
+  function matchName(drivers, customer) {
+    const names = drivers.filter 
+    (driver => driver.name === customer);
+    return names;
+    }
